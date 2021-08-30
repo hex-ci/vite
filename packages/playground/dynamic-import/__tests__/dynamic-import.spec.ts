158e601335edea66bf253141784e211ce3fa0e54
Ruicong <crcong@foxmail.com>
2021-08-30 21:55:11 +0800

    fix: dynamic import css test case

--- packages/playground/dynamic-import/__tests__/dynamic-import.spec.ts
+++ packages/playground/dynamic-import/__tests__/dynamic-import.spec.ts
@@ -50,8 +50,8 @@ test('should load dynamic import with vars', async () => {
 test('should load dynamic import with css', async () => {
   await page.click('.css')
   await untilUpdated(
-    () => page.$eval('.css', (node) => window.getComputedStyle(node).boxSizing),
-    'border-box',
+    () => page.$eval('.css', (node) => window.getComputedStyle(node).display),
+    'inline-flex',
     true
   )
 })
